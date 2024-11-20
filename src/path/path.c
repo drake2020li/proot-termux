f4573eaea193bc1ccf1f1c0ef5737962ec799a18
drake2020li <69410229+drake2020li@users.noreply.github.com>
2024-11-21 00:59:25 +0800

    Update path.c

--- src/path/path.c
+++ src/path/path.c
@@ -543,10 +543,6 @@ Comparison compare_paths2(const char *path1, size_t length1, const char *path2, 
 	assert(length(path1) == length1);
 	assert(length(path2) == length2);
 #endif
-	printf("path1(%s), length1(%zu)\n", path1, length1);
-	printf("path2(%s), length2(%zu)\n", path2, length2);
-	assert(length1 > 0);
-	assert(length2 > 0);
 
 	if (!length1 || !length2) {
 		return PATHS_ARE_NOT_COMPARABLE;
